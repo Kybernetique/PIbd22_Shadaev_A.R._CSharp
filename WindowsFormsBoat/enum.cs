8a1a784b794f760ba6fb9a33f80c41a78edceac1
Tony <54847558+Kybernetique@users.noreply.github.com>
2021-10-19 00:46:45 +0400

    Начало работы с GitHub

---
+++ WindowsFormsBoat/enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsBoat
+{
+    public enum Direction
+    {
+        Up,
+
+        Down,
+
+        Left,
+
+        Right
+    }
+}
