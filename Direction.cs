e231905ced395ba6d2ba5310e15d905a8e9a275d
Tony <54847558+Kybernetique@users.noreply.github.com>
2021-10-28 22:06:15 +0400

    Пересоздал файл Direction.cs, т.к. была ошибка

--- Direction.cs
+++ Direction.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Drawing;
-using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WindowsFormsBoat
 {
     public enum Direction
     {
         Up,
-
         Down,
-
         Left,
-
         Right
     }
 }
